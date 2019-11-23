4ee582633375f79912fa1023868c6d3fa1c6e168
Logan Negley <logannegley@gmail.com>
2019-11-22 21:54:55 -0600

    added info to cards

--- starwars/src/components/WarCard.js
+++ starwars/src/components/WarCard.js
@@ -1,7 +1,7 @@
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const 
+
 
 
 
@@ -11,9 +11,15 @@ const
 const WarCard = props => {
   return (
     <div className="war-card ">
-  <h1>
-    {props.eachCharacter.name}
-  </h1>
+  <h1>{props.eachCharacter.name}</h1>
+<ul>
+ <li>Gender: {props.eachCharacter.gender}</li>
+  <li>Year Born: {props.eachCharacter.birth_year}</li>
+  <li>Eye Color: {props.eachCharacter.eye_color}</li>
+   <li>Hair Color: {props.eachCharacter.hair_color}</li>
+   
+</ul>
+
 
     </div>
   );
