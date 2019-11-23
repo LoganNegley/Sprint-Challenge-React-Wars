b4fb77f68c405b6e7916da31756bc0d2f063ea1e
Logan Negley <logannegley@gmail.com>
2019-11-22 20:37:59 -0600

    added axios and started mapping through data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from "axios";
+
+
+
+
 
 const App = () => {
+
+  const [characterData, setCharacterData]= useState({})
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,10 +16,28 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios
+      .get(`https://swapi.co/api/people/`)
+      .then(response => {
+        console.log(response);
+        setCharacterData(response.data.results);
+      });
+  }, []);
+
   return (
+    <>
     <div className="App">
       <h1 className="Header">React Wars</h1>
     </div>
+    <div>
+      {
+        characterData.map(item,index) => {
+          return
+        }
+      }
+    </div>
+    </>
   );
 }
 
